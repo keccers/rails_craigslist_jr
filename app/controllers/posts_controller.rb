81ad96260d4794fe331627f4dbd150d8beb6fd30
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 16:40:50 -0500

    added form create and edit functionality (mind blown)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,37 @@
 class PostsController < ApplicationController
+  before_filter :load_category
 
-def index
-  @posts = Post.where(category_id: params[:category_id])
-  @category = Category.find(params[:category_id])
+  def index
+    @posts = @category.posts #Post.where(category_id: params[:category_id])
+  end
+
+  def create
+    @post = @category.posts.new(params[:post])
+    @post.save
+
+    redirect_to category_posts_path
+  end
+
+  def new
+    @post = @category.posts.new
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def load_category
+    @category = Category.find(params[:category_id])
+  end
 end
-
-def create
-  @post = Post.new(params[:post])
-  @post.category_id = params[:category_id]
-  @post.save
-
-  redirect_to category_posts_path
-end
-
-def new
-  @post = Post.new
-end
-
-def edit
-end
-
-def show
-end
-
-def update
-end
-
-def destroy
-end
-
-end
-
