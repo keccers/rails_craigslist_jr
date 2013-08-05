0761b20c225335a8577a8de6e626714d4ad9e693
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 15:07:22 -0500

    Add collection logic for index route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,18 @@
 class PostsController < ApplicationController
 
 def index
+  @posts = Post.where(category_id: params[:category_id])
+  @category = Category.find(params[:category_id])
 end
 
 def create
+
+  #pulling data to create post
 end
 
 def new
+
+  #render the form
 end
 
 def edit
