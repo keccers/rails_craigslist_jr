cb9879e7ba1607304e829ddba2900c2a97abf106
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 14:31:19 -0500

    Add before validation callback for post's key attribute

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :text, :email, :key, :price, :category_id
   belongs_to :category
   validates_presence_of :title, :text, :email, :key
+  before_validation :create_key, on: :create
+
+  def create_key
+    self.key = (0...8).map{(65+rand(26)).chr}.join
+  end
 end
